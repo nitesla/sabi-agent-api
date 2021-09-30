8c685b96f16dc3eeaa3b9a6988dc8e0529fe8d1f
Davyjones <ekenedavidson15@gmail.com>
2021-09-30 06:06:54 +0100

    merged

--- src/main/java/com/sabi/agent/api/controller/AgentVerificationController.java
+++ src/main/java/com/sabi/agent/api/controller/AgentVerificationController.java
@@ -63,7 +63,7 @@ public class AgentVerificationController {
     public ResponseEntity<Response> verification(@Validated @RequestBody Verification request){
         HttpStatus httpCode ;
         Response resp = new Response();
-        service.changeVerificationStatus(request);
+        service.verification(request);
         resp.setCode(CustomResponseCode.SUCCESS);
         resp.setDescription("Successful");
         httpCode = HttpStatus.OK;
