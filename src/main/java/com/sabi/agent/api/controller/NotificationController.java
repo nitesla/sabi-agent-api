2a7602a18eed3bdd475e2448226a7eddb6c72797
Davyjones <ekenedavidson15@gmail.com>
2021-10-06 18:59:41 +0100

    new updates

--- src/main/java/com/sabi/agent/api/controller/NotificationController.java
+++ src/main/java/com/sabi/agent/api/controller/NotificationController.java
@@ -1,64 +1,55 @@
-package com.sabi.agent.api.controller;
-
-
-import com.sabi.framework.notification.requestDto.NotificationRequestDto;
-import com.sabi.framework.notification.responseDto.NotificationResponseDto;
-import com.sabi.framework.service.NotificationService;
-import com.sabi.framework.utils.Constants;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-
-
-@SuppressWarnings("All")
-@RestController
-@Validated
-@Valid
-@RequestMapping(Constants.APP_CONTENT +"notification")
-public class NotificationController {
-
-    private final NotificationService service;
-
-    public NotificationController(NotificationService service) {
-        this.service = service;
-    }
-
-    /** <summary>
-     *  Email Notification endpoint
-     * </summary>
-     * <remarks>this endpoint is responsible for email notification</remarks>
-     */
-
-
-    @PostMapping("/email")
-    public NotificationResponseDto emailNotification (@RequestBody NotificationRequestDto request) throws Exception {
-        System.out.println("::::; EMAIL ::::::"+ request);
-        NotificationResponseDto response= service.emailNotificationRequest(request);
-        return response;
-    }
-
-    /** <summary>
-     *  Sms Notification creation endpoint
-     * </summary>
-     * <remarks>this endpoint is responsible for sms notification</remarks>
-     */
-
-//    @PostMapping("/sms")
-//    public ResponseEntity<Response> sms(@Validated @RequestBody NotificationRequestDto request){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        NotificationRequestDto response = service.smsNotificationRequest(request);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Successful");
-//        resp.setData(response);
-//        httpCode = HttpStatus.CREATED;
-//        return new ResponseEntity<>(resp, httpCode);
+//package com.sabi.agent.api.controller;
+//
+//
+//import com.sabi.framework.notification.requestDto.NotificationRequestDto;
+//import com.sabi.framework.notification.requestDto.SmsRequest;
+//import com.sabi.framework.notification.responseDto.NotificationResponseDto;
+//import com.sabi.framework.service.NotificationService;
+//import com.sabi.framework.utils.Constants;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.validation.Valid;
+//
+//
+//@SuppressWarnings("All")
+//@RestController
+//@Validated
+//@Valid
+//@RequestMapping(Constants.APP_CONTENT +"notification")
+//public class NotificationController {
+//
+//    private final NotificationService service;
+//
+//    public NotificationController(NotificationService service) {
+//        this.service = service;
 //    }
-
-
-
-}
+//
+//
+//
+//
+//    @PostMapping("/email")
+//    public NotificationResponseDto emailNotification (@RequestBody NotificationRequestDto request) throws Exception {
+//        System.out.println("::::; EMAIL ::::::"+ request);
+//        NotificationResponseDto response= service.emailNotificationRequest(request);
+//        return response;
+//    }
+//
+//
+//
+//
+//    @PostMapping("/sms")
+//    public String smsNotification (@RequestBody SmsRequest request) throws Exception {
+//
+//        String response= service.smsNotificationRequest(request);
+//        return response;
+//    }
+//
+//
+//
+//
+//
+//}
