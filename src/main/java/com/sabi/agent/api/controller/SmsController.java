bcfb88050d212577a20cdd837a10fd22b9003b52
lekanspinel <90246896+lekanspinel@users.noreply.github.com>
2021-10-08 23:30:39 +0100

    Dev (#33)
    * update
    
    * Updated pom file to automate library profiling.
    
    * Updated pom file to automate library profiling final
    
    * new jars
    
    * new update
    
    * Added database connection credentials and mysql connector.
    
    * CORS ORIGIN
    
    * Added AgentSupervisorController
    
    * Authentication
    
    * new Jars
    
    * updated jars 14/09/2021
    
    * pom update
    
    * DB update
    
    * +++Api Fixes
    
    * 15/09/21
    
    * 15/09/21 update
    
    * logback update
    
    * update new 15/09/2021
    
    * latest
    
    * log console
    
    * conflict fixed now
    
    * +++Api Fixes
    
    * added get by status endpoint
    
    * +++Api Fixes
    
    * 17/09/21
    
    * fixes
    
    * 17/09/21 fixes
    
    * integration
    
    * +++Api Fixes
    
    * added wallet controller
    
    * +++Api Fixes
    
    * +++Api Fixes
    
    * +++Api Fixes
    
    * update now
    
    * update now 21/09
    
    * 21/09/2021
    
    * MOST RECENT UPDATE
    
    * +++Api Fixes
    
    * +++Api Fixes
    
    * AGENT ID ADDED
    
    * new update 22/09/2021
    
    * fixes
    
    * new fixes
    
    * +++Api Fixes
    
    * FIX
    
    * FIXES
    
    * +++Api Fixes
    
    * 23/09/2021
    
    * latest update
    
    * new update now
    
    * +++Api Fixes
    
    * new update
    
    * 25/09/21
    
    * up to date fixes
    
    * +++Api Fixes
    
    * +++Api Fixes
    
    * notification
    
    * notification today
    
    * 27/09/21
    
    * external token gen
    
    * new jars
    
    * properties file
    
    * 29/09/2021
    
    * merged
    
    * changes to agent bank
    
    * update
    
    * update new
    
    * new push
    
    * Changes to agent bank
    
    * details
    
    * auth
    
    * NEW UPDATE
    
    * fixes  for agent bank
    
    * 06/10/2021
    
    * new jars
    
    * new updates
    
    * fixes 07/10
    
    * verification update
    
    * AGENT BANK UPDATE
    
    * fixes for agent bank
    
    * update
    
    * new update
    
    Co-authored-by: Davyjones <ekenedavidson15@gmail.com>
    Co-authored-by: lekanspinel <lekan.s@spinel-consulting.com>
    Co-authored-by: AtumaKen <atumaken@gmail.com>
    Co-authored-by: Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
    Co-authored-by: Kenoly <kelechi.a@spinel.consulting>

--- src/main/java/com/sabi/agent/api/controller/SmsController.java
+++ src/main/java/com/sabi/agent/api/controller/SmsController.java
@@ -1,44 +1,44 @@
-package com.sabi.agent.api.controller;
-
-
-import com.sabi.agent.core.dto.requestDto.SmsRequestDto;
-import com.sabi.agent.service.integrations.SmsService;
-import com.sabi.framework.utils.Constants;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-
-
-@SuppressWarnings("All")
-@RestController
-@Valid
-@Validated
-@RequestMapping(Constants.APP_CONTENT +"testsms")
-public class SmsController {
-
-    private final SmsService service;
-
-    public SmsController(SmsService service) {
-        this.service = service;
-    }
-
-    /** <summary>
-     *  Sms creation endpoint
-     * </summary>
-     * <remarks>this endpoint is responsible for testing SMS</remarks>
-     */
-
-    @PostMapping("")
-    public SmsRequestDto testSms(@Validated @RequestBody SmsRequestDto request) throws Exception{
-
-        SmsRequestDto response = service.testSms(request);
-        return response;
-    }
-
-
-
-}
+//package com.sabi.agent.api.controller;
+//
+//
+//import com.sabi.agent.core.dto.requestDto.SmsRequestDto;
+//import com.sabi.agent.service.integrations.SmsService;
+//import com.sabi.framework.utils.Constants;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.validation.Valid;
+//
+//
+//@SuppressWarnings("All")
+//@RestController
+//@Valid
+//@Validated
+//@RequestMapping(Constants.APP_CONTENT +"testsms")
+//public class SmsController {
+//
+//    private final SmsService service;
+//
+//    public SmsController(SmsService service) {
+//        this.service = service;
+//    }
+//
+//    /** <summary>
+//     *  Sms creation endpoint
+//     * </summary>
+//     * <remarks>this endpoint is responsible for testing SMS</remarks>
+//     */
+//
+//    @PostMapping("")
+//    public SmsRequestDto testSms(@Validated @RequestBody SmsRequestDto request) throws Exception{
+//
+//        SmsRequestDto response = service.testSms(request);
+//        return response;
+//    }
+//
+//
+//
+//}
