4a2318e22095450f0cc7eeb26ee2dd9f8c5a5390
Kenoly <kelechi.a@spinel.consulting>
2021-10-20 08:45:32 +0100

    Added MerchantController and WalletController

--- src/main/java/com/sabi/agent/api/controller/MerchantController.java
+++ src/main/java/com/sabi/agent/api/controller/MerchantController.java
@@ -1,11 +1,15 @@
 package com.sabi.agent.api.controller;
 
 import com.sabi.agent.core.merchant_integration.request.MerchantSignUpRequest;
+import com.sabi.agent.core.merchant_integration.response.MerchantOtpResponse;
+import com.sabi.agent.core.merchant_integration.response.MerchantOtpValidationResponse;
 import com.sabi.agent.core.merchant_integration.response.MerchantSignUpResponse;
 import com.sabi.agent.service.services.MerchantService;
 import com.sabi.framework.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping(Constants.APP_CONTENT + "merchant")
@@ -14,9 +18,23 @@ public class MerchantController {
     @Autowired
     MerchantService service;
 
-    @PostMapping
+    @PostMapping("/signUp")
     public MerchantSignUpResponse merchantSignUp(@RequestBody MerchantSignUpRequest request,
                                                  @RequestHeader("fingerprint") String fingerPrint){
         return service.createMerchant(request, fingerPrint);
     }
+
+    @GetMapping("/otp")
+    public MerchantOtpResponse generateOtp(@RequestHeader("fingerprint") String fingerPrint,
+                                           @RequestHeader("phoneNumber") String msisdn) throws UnsupportedEncodingException {
+        return service.sendOtp(fingerPrint, msisdn);
+    }
+
+    @GetMapping("/validateOtp")
+    public MerchantOtpValidationResponse validateOtp(@RequestHeader("fingerprint") String fingerPrint,
+                                                     @RequestHeader("userId") String userId,
+                                                     @RequestHeader("otp") String otp){
+        return service.validateOtp(fingerPrint, userId, otp);
+    }
+
 }
