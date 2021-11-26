8ae7f818cae82ddfaba8705237a1cc6d005edf2b
muyiwa-spinel <muyiwa.a@spinel.consulting>
2021-11-26 11:59:44 -0800

    wish list+

--- src/main/java/com/sabi/agent/api/controller/ProductController.java
+++ src/main/java/com/sabi/agent/api/controller/ProductController.java
@@ -1,49 +1,49 @@
-package com.sabi.agent.api.controller;
-
-
-import com.sabi.agent.core.integrations.request.AllProductsRequest;
-import com.sabi.agent.core.integrations.request.SingleProductRequest;
-import com.sabi.agent.core.integrations.response.AllProductResponse;
-import com.sabi.agent.core.integrations.response.SingleProductResponse;
-import com.sabi.agent.service.integrations.ProductService;
-import com.sabi.framework.utils.Constants;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-
-@SuppressWarnings("All")
-@Slf4j
-@RestController
-@RequestMapping(Constants.APP_CONTENT +"product")
-public class ProductController {
-
-    private final ProductService service;
-
-    public ProductController(ProductService service) {
-        this.service = service;
-    }
-
-
-    /** <summary>
-     * Get single record endpoint
-     * </summary>
-     * <remarks>this endpoint is responsible for getting a single record</remarks>
-     */
-    @PostMapping("")
-    public SingleProductResponse productDetails (@RequestBody SingleProductRequest request) throws Exception {
-        SingleProductResponse response= service.productDetail(request);
-        return response;
-    }
-
-
-    /** <summary>
-     * Get all records endpoint
-     * </summary>
-     * <remarks>this endpoint is responsible for getting all records and its searchable</remarks>
-     */
-    @GetMapping("/list")
-    public AllProductResponse allProductDetails (AllProductsRequest request) throws Exception {
-        AllProductResponse response= service.allProductDetail(request);
-        return response;
-    }
-}
+//package com.sabi.agent.api.controller;
+//
+//
+//import com.sabi.agent.core.integrations.request.AllProductsRequest;
+//import com.sabi.agent.core.integrations.request.SingleProductRequest;
+//import com.sabi.agent.core.integrations.response.AllProductResponse;
+//import com.sabi.agent.core.integrations.response.SingleProductResponse;
+//import com.sabi.agent.service.integrations.ProductService;
+//import com.sabi.framework.utils.Constants;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//@SuppressWarnings("All")
+//@Slf4j
+//@RestController
+//@RequestMapping(Constants.APP_CONTENT +"product")
+//public class ProductController {
+//
+//    private final ProductService service;
+//
+//    public ProductController(ProductService service) {
+//        this.service = service;
+//    }
+//
+//
+//    /** <summary>
+//     * Get single record endpoint
+//     * </summary>
+//     * <remarks>this endpoint is responsible for getting a single record</remarks>
+//     */
+//    @PostMapping("")
+//    public SingleProductResponse productDetails (@RequestBody SingleProductRequest request) throws Exception {
+//        SingleProductResponse response= service.productDetail(request);
+//        return response;
+//    }
+//
+//
+//    /** <summary>
+//     * Get all records endpoint
+//     * </summary>
+//     * <remarks>this endpoint is responsible for getting all records and its searchable</remarks>
+//     */
+//    @GetMapping("/list")
+//    public AllProductResponse allProductDetails (AllProductsRequest request) throws Exception {
+//        AllProductResponse response= service.allProductDetail(request);
+//        return response;
+//    }
+//}
