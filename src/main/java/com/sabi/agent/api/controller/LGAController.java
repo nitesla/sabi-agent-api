fa47ed3cb12e0e826a1367d5a7ad5f0b997cafc7
Davyjones <ekenedavidson15@gmail.com>
2021-12-09 09:34:21 +0100

    new

--- src/main/java/com/sabi/agent/api/controller/LGAController.java
+++ src/main/java/com/sabi/agent/api/controller/LGAController.java
@@ -128,10 +128,11 @@ public class LGAController {
 
 
     @GetMapping("/list")
-    public ResponseEntity<Response> getAll(@RequestParam(value = "isActive")Boolean isActive){
+    public ResponseEntity<Response> getAll(@RequestParam(value = "isActive")Boolean isActive,
+                                           @RequestParam(value = "stateId")Long stateId){
         HttpStatus httpCode ;
         Response resp = new Response();
-        List<LGA> response = service.getAll(isActive);
+        List<LGA> response = service.getAll(isActive,stateId);
         resp.setCode(CustomResponseCode.SUCCESS);
         resp.setDescription("Record fetched successfully !");
         resp.setData(response);
